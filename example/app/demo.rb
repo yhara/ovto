576e7c006896dd1230320e54bad9154e49c1c8d5
Yutaka HARA <yutaka.hara+github@gmail.com>
2018-04-19 19:40:41 +0900

    Update demo

--- example/app/demo.rb
+++ example/app/demo.rb
@@ -1,4 +1,27 @@
 require 'opal'
 require 'ovto'
 
+class TodoApp < Ovto::App
+  class State < Ovto::State
+    item :todos, []
+    item :filter, :all
+    item :input, ""
+  end
+
+  module Actions
+    def add_todo(state)
+
+    end
+  end
+
+  class View < Ovto::Component
+    def render(state)
+      o 'div' do
+        o 'h1', {}, 'Todo'
+      end
+    end
+  end
+end
+
+Ovto.run(TodoApp)
 p 1
--- example/config.ru
+++ example/config.ru
@@ -6,6 +6,7 @@ run Opal::Sprockets::Server.new{|s|
   # Let javascript_include_tag to serve compiled version of opal/dxopal.rb
   s.append_path 'app'
   s.main = 'demo'
+  s.index_path = 'index.html.erb'
   # Serve static files
   #s.public_root = __dir__
   # Just serve static ./index.html
