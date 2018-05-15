f9feb1ff40010c661b010bf39221018cf8a4d5b1
Yutaka HARA <yutaka.hara+github@gmail.com>
2018-05-15 10:32:03 +0900

    Add static example

---
+++ example/static/app.rb
@@ -0,0 +1,23 @@
+require 'opal'
+require 'ovto'
+
+class MyApp < Ovto::App
+  class State < Ovto::State
+    item :count, default: 0
+  end
+
+  class Actions < Ovto::Actions
+    def increment(state:, by:)
+      return {count: state.count + by}
+    end
+  end
+
+  class View < Ovto::Component
+    def render(state)
+      o 'span', state.count
+      o 'button', onclick: ->{ actions.increment(by: 1) }
+    end
+  end
+end
+
+MyApp.run(id: 'ovto-view')
