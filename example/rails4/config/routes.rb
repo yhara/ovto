5c332f16ed01e3c6e53b7ef166c4f4ea5d305335
Yutaka HARA <yutaka.hara+github@gmail.com>
2018-05-24 20:33:45 +0900

    generate controller

--- example/rails4/config/routes.rb
+++ example/rails4/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'top#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
