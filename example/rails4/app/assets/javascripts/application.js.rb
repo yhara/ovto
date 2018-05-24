0435efb5ff46ac08cdd28c5e230947842b195a97
Yutaka HARA <yutaka.hara+github@gmail.com>
2018-05-24 20:34:14 +0900

    Install ovto and ovto app

---
+++ example/rails4/app/assets/javascripts/application.js.rb
@@ -0,0 +1,4 @@
+require 'opal'
+#require 'opal_ujs'
+#require 'turbolinks'
+require_tree '.'
--- example/rails4/config/initializers/assets.rb
+++ example/rails4/config/initializers/assets.rb
@@ -9,3 +9,12 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Opal.use_gem 'ovto'
+# https://github.com/opal/opal-rails#configuration
+Rails.application.config.opal.method_missing           = true
+Rails.application.config.opal.optimized_operators      = true
+Rails.application.config.opal.arity_check              = !Rails.env.production?
+Rails.application.config.opal.const_missing            = true
+Rails.application.config.opal.dynamic_require_severity = :ignore
+
