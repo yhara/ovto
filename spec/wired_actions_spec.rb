998d53aadb517843fc6ee5e6d097f75bedf1f4d4
Yutaka HARA <yutaka.hara+github@gmail.com>
2018-08-16 20:39:16 +0900

    test: Add test

---
+++ spec/wired_actions_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+module Ovto
+  describe WiredActions do
+    class AppExample < Ovto::App
+      class State < Ovto::State
+        item :ct, default: 0
+      end
+
+      class Actions < Ovto::Actions
+        def increment_counter(state:)
+          return {ct: state.ct + 1}
+        end
+      end
+    end
+
+    it 'has proxy methods to actions' do
+      app = Object.new
+      runtime = Object.new
+      wired_actions = WiredActions.new(AppExample::Actions.new, app, runtime)
+      state = AppExample::State.new
+
+      allow(app).to receive(:state).and_return(state)
+      expect(runtime).to receive(:scheduleRender)
+      expect(app).to receive(:_set_state).with(AppExample::State.new(ct: 1))
+
+      ret = wired_actions.increment_counter(state: state)
+      expect(ret).to eq(AppExample::State.new(ct: 1))
+    end
+  end
+end
