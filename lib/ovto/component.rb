f184f7ac07b3d0ba9f229aa6738edd6dcd4ef226
Yutaka HARA <yutaka.hara+github@gmail.com>
2018-08-16 20:00:02 +0900

    fix: Fix typo

--- lib/ovto/component.rb
+++ lib/ovto/component.rb
@@ -125,7 +125,7 @@ module Ovto
 
     def render_component(comp_class, args, children)
       comp = comp_class.new(@wired_actions)
-      return comp.render(**{state: args){ children }
+      return comp.render(**{state: args}){ children }
     end
 
     def render_tag(tag_name, attributes, children)
